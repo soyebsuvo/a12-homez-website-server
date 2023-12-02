d1731fa67b14f5b46ede023d9d1a2f12783e6736
soyebsuvo <soyebbeen@gmail.com>
2023-12-02 12:29:06 +0600

    wishlistCount api added

--- index.js
+++ index.js
@@ -55,6 +55,10 @@ async function run() {
       const result = await wishlistCollection.insertOne(item);
       res.send(result);
     })
+    app.get("/wishlistCount" , async (req , res) => {
+      const count = await wishlistCollection.estimatedDocumentCount();
+      res.send({count})
+    })
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
