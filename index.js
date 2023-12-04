d6928fe865bac6760067e7922f5a335188cf5ab7
soyebsuvo <soyebbeen@gmail.com>
2023-12-04 15:29:35 +0600

    console cleared

--- index.js
+++ index.js
@@ -429,7 +429,7 @@ async function run() {
     })
     // Connect the client to the server	(optional starting in v4.7)
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
